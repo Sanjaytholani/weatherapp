1069f27cbec8c47a1b619516387998aa26524f2f
Sanjaytholani <sanjaytholani.m@gmail.com>
2020-06-19 13:12:10 +0530

    Complete1

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,7 @@
 const key = 'f0czKfEVGT4QgwUY8GWiJeEeAnL0w6S9';
 
 const getWeather = async (id) => {
-    const base='http://dataservice.accuweather.com/currentconditions/v1/';
+    const base='https://dataservice.accuweather.com/currentconditions/v1/';
     const query=`${id}?apikey=${key}`
     const response=await fetch(base+query);
     const data=await response.json();
@@ -10,7 +10,7 @@ const getWeather = async (id) => {
 
 
 const getCity = async(city)=>{
-    const base='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base='https://dataservice.accuweather.com/locations/v1/cities/search';
     const query=`?apikey=${key}&q=${city}`;
     const response = await fetch(base+query);
     const data= await response.json();
